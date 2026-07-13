198b4d961be9e2f5f7d419253ad86bfaaaae1a62
Boscher-audrey <boscher.audrey@gmail.com>
2026-07-13 12:59:43 +0200

    Add month & year navigation

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,27 @@ import './App.css'
 import { useState } from 'react'
 
 function App() {
-  const [todaysDate, setTodaysDate] = useState<Date>(new Date())
+  const [displayedDate, setDisplayedDate] = useState<Date>(new Date())
+  const changeDate = (offsetMonth: number, offsetYear: number) => {
+    setDisplayedDate(
+      new Date(displayedDate.getFullYear() + offsetYear, displayedDate.getMonth() + offsetMonth, 1),
+    )
+  }
 
   return (
     <>
       <section id="center">
         <div>
           <h1>Application de calendrier</h1>
-          <p>{todaysDate.toLocaleString('fr-FR', { month: 'long', year: 'numeric' })}</p>
+          <p>{displayedDate.toLocaleString('fr-FR', { month: 'long', year: 'numeric' })}</p>
+        </div>
+        <div>
+          <button onClick={() => changeDate(-1, 0)}>Mois précédent</button>
+          <button onClick={() => changeDate(1, 0)}>Mois suivant</button>
+        </div>
+        <div>
+          <button onClick={() => changeDate(0, -1)}>Année précédente</button>
+          <button onClick={() => changeDate(0, 1)}>Année suivante</button>
         </div>
       </section>
     </>
